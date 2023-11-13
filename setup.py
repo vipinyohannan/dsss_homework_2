2b1be9b72e974869b8c3a1d1b4de5b90357c81bc
vipinyohannan <vipinyohannan8@gmail.com>
2023-11-13 23:04:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your project might have
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz:math_quiz',
+        ],
+    },
+)
 
